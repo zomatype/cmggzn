469abf52b4d61d4c8f61e52e697af701308870b8
zo-ma <zero.one.mean.ability@outlook.jp>
2023-06-14 14:35:43 +0900

    add

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+const canvas = document.querySelector("#draw-area");
+const context = canvas.getContext("2d");
+
+let lineColor = "#9F6345";
+
+canvas.addEventListener("mousemove", (event) => {
+  draw(event.layerX, event.layerY);
+});
+canvas.addEventListener("touchmove", (event) => {
+  draw(event.layerX, event.layerY);
+});
+
+canvas.addEventListener("mousedown", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("mouseup", () => {
+  context.closePath();
+  isDrag = false;
+});
+canvas.addEventListener("touchstart", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("touchend", () => {
+  context.closePath();
+  isDrag = false;
+});
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+let isDrag = false;
+function draw(x, y) {
+  if (!isDrag) {
+    return;
+  }
+
+  context.lineWidth = 5;
+  context.strokeStyle = lineColor;
+  context.lineTo(x, y);
+  context.stroke();
+}
